f726521e37c5157fd57e3dc24a4d4ab95f93414f
Brad Toney <bravotango87@gmail.com>
2020-05-05 12:44:18 -0400

    created classes and lists for  soda & money, and put the lists in the SodaMachine class.

--- SodaMachine.cs
+++ SodaMachine.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SodaMachine;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,15 +8,20 @@ namespace SodaMachineOOP
     class SodaMachine
     {
         // member variables (Has a)
-      
 
+        Inventory inventory;
+        MoneyHolder moneyHolder;
 
 
 
 
         //  constructor (Spawner)
 
-       
+      public SodaMachine()
+        {
+            inventory = new Inventory();
+            moneyHolder = new MoneyHolder();
+        }
         
 
 
