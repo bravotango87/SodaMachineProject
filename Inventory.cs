f34b0f5ecc3345100df1f6d992f7ca481f86baff
Brad Toney <bravotango87@gmail.com>
2020-05-05 12:27:31 -0400

    added Inventory and MoneyHolder classes to hold lists of the money and the sodas.

---
+++ Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SodaMachine
+{
+    class Inventory // sodas
+    {
+        // member variables (Has a)
+
+
+        public List<LemonSoda> lemonSodas;
+        public List<OrangeSoda> orangeSodas;
+        public List<GrapeSoda> grapeSodas;
+
+
+      // constructor (Spawner)
+
+      public Inventory()
+        {
+            lemonSodas = new List<LemonSoda>();
+            orangeSodas = new List<OrangeSoda>();
+            grapeSodas = new List<GrapeSoda>();
+            FillMachine();
+              
+        }
+
+     // member methods (Can do)
+
+       public void FillMachine()
+        {
+
+        }
+
+    }
+}
---
+++ MoneyHolder.cs
@@ -0,0 +1,46 @@
+﻿using SodaMachineOOP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SodaMachine
+{
+    class MoneyHolder // coins
+    {
+        // member variables (Has a)
+
+        public List<Penny> pennies;
+        public List<Dime> dimes;
+        public List<Nickel> nickels;
+        public List<Quarter> quarters;
+
+
+
+
+
+
+       // constructor (Spawner) 
+
+        public MoneyHolder()
+        {
+            pennies = new List<Penny>();
+            dimes = new List<Dime>();
+            nickels = new List<Nickel>();
+            quarters = new List<Quarter>();
+            MoneyInMachine();
+            
+        }
+
+     
+   
+
+
+
+      // member methods (Can do)
+
+        public void MoneyInMachine()
+        {
+
+        }
+    }
+}
